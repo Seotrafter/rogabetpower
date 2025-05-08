55b07637918fbda4ccc1b7dbf64c06812dc78a78
Seotrafter <ts@traffter.com>
2025-05-08 11:45:35 +0300

    Add files via upload
    initial commit

---
+++ cookies.js
@@ -0,0 +1,5 @@
+window.addEventListener("load", function () {
+  setTimeout(() => {
+    document.querySelector(".cookie-consent").classList.add("show");
+  }, 1000); // Панель з'являється через 1 секунду після завантаження сторінки
+});
